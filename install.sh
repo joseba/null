7ba259e35940254db57f6a381008ef202501e9d8
joseba <joseba@outlook.com>
2021-06-28 20:34:32 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -78,6 +78,12 @@ echo "Encrypting system partition"
 cryptsetup luksFormat --perf-no_read_workqueue --perf-no_write_workqueue --type luks2 --cipher aes-xts-plain64 --key-size 512 --iter-time 2000 --pbkdf argon2id --hash sha3-512 ${DISK}2
 cryptsetup --allow-discards --perf-no_read_workqueue --perf-no_write_workqueue --persistent open ${DISK}2 crypt
 
+# 
+echo "Formatting the EFI Partition as FAT32."
+mkfs.fat -F 32  &>/dev/null
+
+
+
 
 cryptsetup close crypt
 

f08a616c401294cba61e687b07dab99630541377
joseba <joseba@outlook.com>
2021-06-28 20:17:14 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ fi
 echo "Creating new partition scheme on $DISK."
 sgdisk --clear \
     --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
-    --new=2:0:0 --typecode=3:8300 --change-name=2:SYSTEM \
+    --new=2:0:0 --typecode=2:8300 --change-name=2:SYSTEM \
     $DISK
 
 

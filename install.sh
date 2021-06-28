720e488571617baec0b530d9bfc0fce6b0b7901a
joseba <joseba@outlook.com>
2021-06-28 22:10:58 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -95,7 +95,17 @@ btrfs su cr /mnt/@home &>/dev/null
 btrfs su cr /mnt/@snapshots &>/dev/null
 btrfs su cr /mnt/@var_log &>/dev/null
 
+# Mounting the newly created subvolumes.
+umount /mnt
+echo "Mounting the newly created subvolumes."
 
+mount -o ssd,noatime,space_cache,compress=zstd,subvol=@ $BTRFS /mnt
+mkdir -p /mnt/{home,.snapshots,/var/log,boot}
+mount -o ssd,noatime,space_cache.compress=zstd,autodefrag,discard=async,subvol=@home $BTRFS /mnt/home
+mount -o ssd,noatime,space_cache,compress=zstd,autodefrag,discard=async,subvol=@snapshots $BTRFS /mnt/.snapshots
+mount -o ssd,noatime,space_cache,compress=zstd,autodefrag,discard=async,subvol=@var_log $BTRFS /mnt/var/log
+chattr +C /mnt/var/log
+mount $ESP /mnt/boot/
 
 
 # Mounting the newly created subvolumes.

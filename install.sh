b010224240d7a7012e375df752bf9f11dd21770b
joseba <joseba@outlook.com>
2021-06-29 21:08:27 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -12,41 +12,14 @@ cecho() {
   echo -e "$RED $@ $NC"
 }
 
-
-cecho "Updating mirrorlist..."
-curl -s "https://www.archlinux.org/mirrorlist/?country=ES&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
-
-
-exit
+#cecho "Updating mirrorlist..."
+#curl -s "https://www.archlinux.org/mirrorlist/?country=ES&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 # Update the system clock
 timedatectl set-ntp true
 
 # Cleaning the TTY.
 clear
-
-# Selecting the kernel flavor to install. 
-kernel_selector () {
-    echo "List of kernels:"
-    echo "1) Stable — Vanilla Linux kernel and modules, with a few patches applied."
-    echo "2) Hardened — A security-focused Linux kernel."
-    echo "3) Longterm — Long-term support (LTS) Linux kernel and modules."
-    echo "4) Zen Kernel — Optimized for desktop usage."
-    read -r -p "Insert the number of the corresponding kernel: " choice
-    echo "$choice will be installed"
-    case $choice in
-        1 ) kernel=linux
-            ;;
-        2 ) kernel=linux-hardened
-            ;;
-        3 ) kernel=linux-lts
-            ;;
-        4 ) kernel=linux-zen
-            ;;
-        * ) echo "You did not enter a valid selection."
-            kernel_selector
-    esac
-}
 
 # Checking the microcode to install.
 CPU=$(grep vendor_id /proc/cpuinfo)

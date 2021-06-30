6535b8dff2d0a950aeb3b607c4120259f5709154
joseba <joseba@outlook.com>
2021-06-30 19:15:38 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ mount $EFI /mnt/boot/
 cecho "Installing the base system"
 pacstrap /mnt base base-devel linux intel-ucode linux-headers linux-firmware btrfs-progs \
     dhcpcd iwd openssh \
-    vim mux htop arch-wiki-docs snapper sudo reflector git pkgfile \
+    vim tmux htop arch-wiki-docs snapper sudo reflector git pkgfile \
     zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
 
 cecho "Generating fstab..."

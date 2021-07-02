76e6151315e196229d2e09471fb8f09e468bc2ad
joseba <jsb@null.localdomain>
2021-07-02 22:14:06 +0200

    update

---
+++ .config/fish/functions/config.fish
@@ -0,0 +1,4 @@
+# Defined via `source`
+function config --wraps='/usr/bin/git --gir-dir=$HOME/.cfg/ --work-tree=$HOME' --wraps='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' --description 'alias config=/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+  /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv; 
+end
